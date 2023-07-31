b0a1a0791f198f52015f52a9ea0a19975de7032a
JadenLund <lundjaden@gmail.com>
2023-07-31 02:43:32 -0500

    Adding the base for the carousel

---
+++ carousel/Information.js
@@ -0,0 +1,30 @@
+import Vape2 from "../images/Vape2.png"
+import Vape3 from "../images/Vape3.png"
+import Vape4 from "../images/Vape4.png"
+import Vape5 from "../images/Vape5.png"
+export const productCarouselData = [
+  {
+    title: "Product One",
+    image: Vape2,
+    description:
+      "Discover the perfect vape for an exceptional vaping experience. With its sleek design and comfortable grip, this device offers long-lasting usage and easy customization. Whether you're new to vaping or a seasoned pro, our device combines style and advanced technology for an unmatched experience.",
+  },
+  {
+    title: "Product Two",
+    image: Vape3,
+    description:
+      "Introducing our disposable vape, the ultimate hassle-free solution for a quick and convenient vaping experience. Designed to be compact and lightweight, this throwaway vape is your perfect on-the-go companion for those moments when you need a temporary vaping fix. Simply unbox the disposable vape, enjoy your favorite e-liquid, and dispose of it responsibly once you're done. Despite its disposable nature, this vape doesn't compromise on quality, delivering a smooth draw and flavorful hits that will satisfy your cravings. Whether you're new to vaping or a seasoned vaper looking for a backup option, our disposable vape ensures a satisfying and enjoyable experience with every use.",
+  },
+  {
+    title: "Product Three",
+    image: Vape4,
+    description:
+      "Unleash the full potential of your favorite e-liquids with our flavor-focused vape. Engineered to enhance the taste of every puff, this high-performance device offers advanced temperature control and coil technology, giving you the ability to fine-tune your settings to suit your preferred flavor profile. Its ergonomic design ensures a comfortable grip during extended vaping sessions, while the durable build guarantees its longevity. Indulge in the rich and satisfying clouds that the device produces, providing a flavor-packed vaping experience like no other.",
+  },
+  {
+    title: "Product Four",
+    image: Vape5,
+    description:
+      "Step into the world of vaping with our versatile and powerful vape. Built for those seeking an exceptional vaping adventure, this rugged device is perfect for outdoor enthusiasts and thrill-seekers. Its sturdy construction can withstand the elements, making it an ideal companion for all your adventures. The device's extended battery life ensures you can enjoy uninterrupted vaping sessions, and its intuitive controls make it easy to operate even in challenging conditions. From beginners to experienced vapers, the device caters to all, delivering a reliable and gratifying vaping experience.",
+  },
+]
---
+++ carousel/README.md
@@ -0,0 +1,5 @@
+Creating a carousel from scratch
+
+-Component 1 - all slides together
+-Component 2 - one slide each
+-Information - holds all the info for the slides
